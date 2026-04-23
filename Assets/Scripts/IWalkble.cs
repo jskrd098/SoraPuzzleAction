ea97970da9e75237fe381d78f167c51911b61796
jskrd098 <jskrdkuma@gmail.com>
2026-04-23 23:26:00 +0900

    260423_Update

--- Assets/Scripts/IWalkble.cs
+++ Assets/Scripts/IWalkble.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public interface IWalkable
 {
     public float WalkSpeed { get; }
+    public bool OnGround { get; }
+    public bool OnLadder { get; }
+    public bool InLadder { get; }
 
     public void Walk(Rigidbody2D rb, Vector2 direction);
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 [RequireComponent(typeof(PlayerInput))]
 [RequireComponent(typeof(PlayerMove))]
+[RequireComponent(typeof(PlayerCensor))]
 [RequireComponent(typeof(Rigidbody2D))]
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private PlayerInput _playerInput;
     [SerializeField] private PlayerMove _playerMove;
+    [SerializeField] private PlayerCensor _playerCensor;
     private Rigidbody2D _rb;
     [SerializeField] private float jumpPower = 5f;
 
@@ -14,18 +16,25 @@ public class PlayerController : MonoBehaviour
     {
         _playerInput = GetComponent<PlayerInput>();
         _playerMove = GetComponent<PlayerMove>();
+        _playerCensor = GetComponent<PlayerCensor>();
         _rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         _playerInput.ReadInput();
-        _rb.linearVelocity = new Vector2(_rb.linearVelocityX, _playerInput.JumpInput ? jumpPower : _rb.linearVelocityY);
+        //_rb.linearVelocity = new Vector2(_rb.linearVelocityX, _playerInput.JumpInput ? jumpPower : _rb.linearVelocityY);
     } 
 
     void FixedUpdate()
     {
-        _playerMove.Walk(_rb, _playerInput.MoveInput);
-        _playerMove.Climb(_rb, _playerInput.MoveInput);
+        if(_playerCensor.IsGrounded(_rb.position))
+        {
+            _playerMove.Walk(_rb, _playerInput.MoveInput);
+        }
+        if(_playerCensor.IsOnLadder(_rb.GetComponent<Collider2D>()))
+        {
+            _playerMove.Climb(_rb, _playerInput.MoveInput);
+        }
     }
 }
